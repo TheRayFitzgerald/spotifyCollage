b59e0b2c44b87cda1cdb477a0df90361d8f6675b
Backup <backup@Rays-MacBook-Pro-2.local>
2022-10-17 23:43:58 +0100

    restructure

--- spotifyCollage/settings/base.py
+++ spotifyCollage/settings/base.py
@@ -101,6 +101,7 @@ DATABASES = {
 }
 '''
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -111,7 +112,18 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+'''
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'NAME': 'spotifyCollage',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',                  
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
