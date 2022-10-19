a19946eab2de4aa33089e80837276821596ccb71
Backup <backup@Rays-MacBook-Pro-2.local>
2022-10-19 08:52:35 +0100

    added heroku db settings;

--- spotifyCollage/settings/prod.py
+++ spotifyCollage/settings/prod.py
@@ -9,6 +9,21 @@ ALLOWED_HOSTS = ['.example.com', 'localhost']
 # LOGIN_REDIRECT_URL = '/display_collage'
 LOGIN_REDIRECT_URL = 'http://localhost:3000/'
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'NAME': 'd43cod7f55p4bk',
+        'USER': 'qkfrtsaysebktb',
+        'PASSWORD': '97934df4d8acbbe5a0796ec11dd09fe9aa21bdb818ca58e4f07281aa717d8fb1',
+        'HOST': 'ec2-18-208-55-135.compute-1.amazonaws.com', 
+        'PORT': '5432',                  
+    }
+}
+
+
+
+# SECURITY SETTINGS
+
 SESSION_COOKIE_SECURE = False
 SECURE_SSL_REDIRECT = False
 
