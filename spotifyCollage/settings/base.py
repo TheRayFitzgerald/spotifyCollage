652a31c5df1222f72447edb6ee713a0a0ad4b814
Backup <backup@Rays-MacBook-Pro-2.local>
2022-10-19 15:32:48 +0100

    django DB settings;

--- spotifyCollage/settings/base.py
+++ spotifyCollage/settings/base.py
@@ -111,16 +111,6 @@ DATABASES = {
 }
 '''
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
-        'NAME': 'spotifyCollage',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',                  
-    }
-}
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
--- spotifyCollage/settings/dev.py
+++ spotifyCollage/settings/dev.py
@@ -4,3 +4,15 @@ DEBUG = True
 
 # LOGIN_REDIRECT_URL = '/display_collage'
 LOGIN_REDIRECT_URL = 'http://localhost:3000/'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'NAME': 'spotifyCollage',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',                  
+    }
+}
