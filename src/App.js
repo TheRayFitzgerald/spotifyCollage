34f7198e69d615631a2498701b1677bce9e3b522
Backup <backup@Rays-MacBook-Pro-2.local>
2022-11-03 22:59:59 +0000

    twitter card meta tags and tweet button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
+import React, { useState, useEffect } from 'react';
 import { Container } from "./styles/App.styles"
+import {Helmet} from "react-helmet";
 import Cookies from 'js-cookie'
 import { SpotifyAuth, Scopes } from 'react-spotify-auth'
 import 'react-spotify-auth/dist/index.css'
-import React, { useState, useEffect } from 'react';
 import { SpotifyAuthContainer, ImageContainer } from "./styles/App.styles"
 import axios from "axios";
 import { Dots } from 'loading-animations-react';
 import Header from "./components/Header";
 import Footer from './components/Footer';
 import './css/App.scss';
+import { TwitterShareButton, TwitterIcon } from 'react-share';
+
 
 
 const App = () => {
+
   const [token, setToken] = React.useState(Cookies.get("spotifyAuthToken"))
   const [collage, setCollage] = useState('');
 
@@ -38,14 +42,29 @@ const App = () => {
     
   return (
     <Container>
+      <Helmet>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@nytimes" />
+        <meta name="twitter:creator" content="@SarahMaslinNir" />
+        <meta name="twitter:title" content="Parade of Fans for Houston’s Funeral" />
+        <meta name="twitter:description" content="NEWARK - The guest list and parade of limousines with celebrities emerging from them seemed more suited to a red carpet event in Hollywood or New York than than a gritty stretch of Sussex Avenue near the former site of the James M. Baxter Terrace public housing project here." />
+        <meta name="twitter:image" content="http://graphics8.nytimes.com/images/2012/02/19/us/19whitney-span/19whitney-span-articleLarge.jpg" />
+      </Helmet>
       <Header></Header>
       {token ? (
         <ImageContainer>
           {collage ? (
-          // Display the collage
-          // <h1 className={"font-bold text-" + fontSize}>Hello World</h1>
-          <img src={"data:image/png;base64," + collage['collage']['img_str']} alt="collage" style={{ alignSelf: 'center' }} />
-          // <img src={collage['collage']['img']} alt="col99age" style={{ alignSelf: 'center' }} />
+            // Display the collage
+            //<h1 className={"font-bold text-" + fontSize}>Hello World</h1>
+            // <img src={collage['collage']['img']} alt="col99age" style={{ alignSelf: 'center' }} />
+    
+            <ImageContainer>
+              <img src={"data:image/png;base64," + collage['collage']['img_str']} alt="collage" style={{ alignSelf: 'center' }} />
+              
+              <div className="tweet-div">
+                <a  href="https://twitter.com/intent/tweet?text=Hello%20world">Tweet</a>
+              </div>
+            </ImageContainer>
         ) : (
           // Display the loading dots
           <Dots id="loadingDots" text=""/>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
         <Box>
         <SignatureContainer>Created by <a href="https://twitter.com/rincidium" style={{color:"black"}}>Ray Fitzgerald</a><br></br></SignatureContainer>
             <SocialIconContainer>
-                <SocialIcon url="https://twitter.com/rincidium" class=".mt-0" style={{ height: 29, width: 30, marginRight: "15px" }} bgColor="white"/>
+                <SocialIcon url="https://twitter.com/rincidium" style={{ height: 29, width: 30, marginRight: "15px" }} bgColor="white"/>
                 <SocialIcon url="https://github.com/TheRayFitzgerald" style={{ height: 30, width: 30, marginRight: "15px" }} bgColor="white"/>
                 <SocialIcon url="https://www.linkedin.com/in/ray-fitzgerald-86b451148/" style={{ height: 30, width: 30 }} bgColor="white" />
             </SocialIconContainer>
